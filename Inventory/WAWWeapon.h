76911b8637ea98ceb8930b5e168c7ff232b8d0c2
psyrus <revolution.x@gmail.com>
2015-05-30 22:40:13 +0900

    I added the fstruct data from Kory's WAW file

--- Inventory/WAWWeapon.h
+++ Inventory/WAWWeapon.h
@@ -12,6 +12,82 @@
 
 class AWAWLoadout;
 
+USTRUCT()
+struct FWeaponData
+{
+	GENERATED_BODY()
+
+		/** inifite ammo for reloads */
+		UPROPERTY(EditDefaultsOnly, Category = Ammo)
+		bool bInfiniteAmmo;
+
+	/** infinite ammo in Mag, no reload required */
+	UPROPERTY(EditDefaultsOnly, Category = Ammo)
+		bool bInfiniteMag;
+
+	/** should this weapon be put back to safe upon reload? good for grenades */
+	UPROPERTY(EditDefaultsOnly, Category = Ammo)
+		bool bGoToSafeOnReload;
+
+	/** max ammo */
+	UPROPERTY(EditDefaultsOnly, Category = Ammo)
+		int32 MaxAmmo;
+
+	/** Mag size */
+	UPROPERTY(EditDefaultsOnly, Category = Ammo)
+		int32 AmmoPerMag;
+
+	/** initial Mags */
+	UPROPERTY(EditDefaultsOnly, Category = Ammo)
+		int32 InitialMags;
+
+	/** time between two consecutive shots */
+	UPROPERTY(EditDefaultsOnly, Category = WeaponStat)
+		float TimeBetweenShots;
+
+	/** failsafe equip duration if weapon doesn't have any animation for it */
+	UPROPERTY(EditDefaultsOnly, Category = WeaponStat)
+		float NoAnimEquipDuration;
+
+	/** failsafe reload duration if weapon doesn't have any animation for it */
+	UPROPERTY(EditDefaultsOnly, Category = WeaponStat)
+		float NoAnimReloadDuration;
+
+	/** minimum recoil amount in degrees to move player's pitch */
+	UPROPERTY(EditDefaultsOnly, Category = WeaponStat)
+		float RecoilPitchMin;
+
+	/** maximum recoil amount in degrees to move player's pitch */
+	UPROPERTY(EditDefaultsOnly, Category = WeaponStat)
+		float RecoilPitchMax;
+
+	/** minimum recoil amount in degrees to move player's yaw */
+	UPROPERTY(EditDefaultsOnly, Category = WeaponStat)
+		float RecoilYawMin;
+
+	/** maximum recoil amount in degrees to move player's yaw */
+	UPROPERTY(EditDefaultsOnly, Category = WeaponStat)
+		float RecoilYawMax;
+
+	/** defaults */
+	FWeaponData()
+	{
+		bInfiniteAmmo = false;
+		bInfiniteMag = false;
+		bGoToSafeOnReload = false;
+		MaxAmmo = 100;
+		AmmoPerMag = 20;
+		InitialMags = 4;
+		TimeBetweenShots = 0.1f; //200 ms between shots => 300 rounds per minute
+		NoAnimEquipDuration = 0.5f;
+		NoAnimReloadDuration = 1.0f;
+		RecoilPitchMin = 0.5f;
+		RecoilPitchMax = 2.f;
+		RecoilYawMin = -1.f;
+		RecoilYawMax = 1.f;
+	}
+};
+
 UENUM(BlueprintType)		//"BlueprintType" is essential to include
 enum class EFireMode : uint8
 {
@@ -27,24 +103,28 @@ class AWAWWeapon : public AWAWUsableItem
 
 public:
 	AWAWWeapon(const FObjectInitializer& ObjectInitializer);
-	virtual void PostInitializeComponents() override;
+		virtual void PostInitializeComponents() override;
 
 	UFUNCTION(BlueprintCallable, Category = Gun)
-	void Fire();
+		void Fire();
 
 	UFUNCTION(BlueprintCallable, Category = Gun)
-	void Reload();
+		void Reload();
 
 	UFUNCTION(BlueprintCallable, Category = Gun)
-	void BurstFire(uint8 RoundsToFire);
+		void BurstFire(uint8 RoundsToFire);
 
 	bool CanFire();
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=Gun)
-	uint8 shotsInMagazine;
+		uint8 shotsInMagazine;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Gun)
-	TArray<EFireMode> fireModes;
-	
+		TArray<EFireMode> fireModes;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Gun)
+		TSubclassOf<AWAWMagazine> magazineType;
+
+	TArray<int8> magazineAmmoCounts;
 
 };
